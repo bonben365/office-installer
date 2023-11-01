660db83d184bac41b0f4cf4806bda903dd08d989
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-01 13:44:31 +0700

    Update uninstaller.ps1

--- uninstaller.ps1
+++ uninstaller.ps1
@@ -2,19 +2,21 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
     break
 }
-
-
-
-$uninstall = {
-   New-Item -Path $env:temp\c2r -ItemType Directory -Force
-   Set-Location $env:temp\c2r
-   $fileName = 'configuration.xml'
-   New-Item $fileName -ItemType File -Force
-   Add-Content $fileName -Value '<Configuration>'
-   Add-Content $fileName -Value '<Remove All="True"/>'
-   Add-Content $fileName -Value '</Configuration>'
-   $uri = 'https://github.com/bonben365/office-installer/raw/main/setup.exe'
-   (New-Object Net.WebClient).DownloadFile($uri, "$env:temp\c2r\setup.exe")
-   .\setup.exe /configure .\configuration.xml
+Write-Host
+Write-Host "Remove all installed Office apps using Office Deployment Tool ODT." -ForegroundColor yellow
+$Confirm= Read-Host Are you sure you want to uninstall all Office apps? [Y] Yes [N] No
+if($Confirm -match "[yY]") {
+    New-Item -Path $env:temp\c2r -Type Directory -Force | Out-Null
+    Set-Location $env:temp\c2r
+    $fileName = 'configuration.xml'
+    New-Item $fileName -ItemType File -Force | Out-Null
+    Add-Content $fileName -Value '<Configuration>'
+    Add-Content $fileName -Value '<Remove All="True"/>'
+    Add-Content $fileName -Value '</Configuration>'
+    $uri = 'https://github.com/bonben365/office-installer/raw/main/setup.exe'
+    (New-Object Net.WebClient).DownloadFile($uri, "$env:temp\c2r\setup.exe")
+    Write-host "Removing the Office apps"
+    .\setup.exe /configure .\configuration.xml
+} else {
+    Write-Host ""
 }
-
