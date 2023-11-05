e96825d2efabcf4d4172c7af2f7987f786c33d65
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-05 19:41:27 +0700

    Update download-all.ps1

--- download-all.ps1
+++ download-all.ps1
@@ -1,7 +1,7 @@
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
     break
- }
+}
 
 $uri = "https://github.com/bonben365/office-installer/raw/main/setup.exe"
 $uri2013 = "https://github.com/bonben365/office-installer/raw/main/bin2013.exe"
@@ -9,14 +9,23 @@ $activator = 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/MSGANG/scripts/office/a
 $readme = 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/MSGANG/scripts/office/Readme.txt'
 $link = 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/MSGANG/scripts/office/Microsoft%20products%20for%20FREE.html'
 
-$edition = $(Write-Host -NoNewLine) + $(Write-Host "Please enter the Office edition (2013/2016/2019/2021/365):" -ForegroundColor Green -NoNewLine; Read-Host)
+Write-Host 
+Write-Host "- Description:****************************************************
+- This tool downloads both x32 and x64 versions of Office apps.  *
+- Support download both Retail and Volume license type.          *
+- Default language for downloading is English.                   *
+- You can change it from the code to download your favorite one. *
+- Download location C:\Users\{username}\Desktop\Source.          *
+******************************************************************" -ForegroundColor Cyan
+
+$edition = $(Write-Host -NoNewLine) + $(Write-Host "`nPlease type the Office edition that you want to download (2013/2016/2019/2021/365):" -ForegroundColor Green -NoNewLine; Read-Host)
 $licType = $(Write-Host -NoNewLine) + $(Write-Host "Please enter the license type (Volume/Retail):" -ForegroundColor Green -NoNewLine; Read-Host)
 
 $archs = @('32';'64')
 $languageId = 'en-US'
 $mode = '/download'
 
-function Install-Office {
+function Download-MSOffice {
     New-Item -Path $env:userprofile\Desktop\Source\$productId$edition-x$arch -ItemType Directory -Force | Out-Null
     Set-Location $env:userprofile\Desktop\Source\$productId$edition-x$arch
     $totalItems = ($productIds.Count)*2
@@ -42,7 +51,7 @@ function Install-Office {
     .\ClickToRun.exe $mode .\$configurationFile
 }
 
-function Install-OfficeX {
+function Download-MSOfficeX {
     New-Item -Path $env:userprofile\Desktop\Source\$productId$edition-x$arch -ItemType Directory -Force | Out-Null
     Set-Location $env:userprofile\Desktop\Source\$productId$edition-x$arch
     $totalItems = ($productIds.Count)*2
@@ -67,23 +76,6 @@ function Install-OfficeX {
     .\ClickToRun.exe $mode .\$configurationFile  
 }
 
-$productIds = @(
-    "ProPlus$edition$licType";
-    "Standard$edition$licType";
-    "ProjectPro$edition$licType";
-    "ProjectStd$edition$licType";
-    "VisioPro$edition$licType";
-    "VisioStd$edition$licType";
-    "Word$edition$licType";
-    "Excel$edition$licType";
-    "PowerPoint$edition$licType";
-    "Outlook$edition$licType";
-    "Publisher$edition$licType";
-    "Access$edition$licType";
-    "HomeBusiness$($edition)Retail";
-    "HomeStudent$($edition)Retail"
-)
-
 $i = 1
 if ($edition -match '2013' -or $edition -match '2016'){
     $productIds = @(
@@ -102,16 +94,32 @@ if ($edition -match '2013' -or $edition -match '2016'){
     )
     foreach ($productId in $productIds) {
         foreach ($arch in $archs){
-            Install-OfficeX
+            Download-MSOfficeX
             $i++
         }
     }
 }
 
 if ($edition -match '2019' -or $edition -match '2021'){
+    $productIds = @(
+    "ProPlus$edition$licType";
+    "Standard$edition$licType";
+    "ProjectPro$edition$licType";
+    "ProjectStd$edition$licType";
+    "VisioPro$edition$licType";
+    "VisioStd$edition$licType";
+    "Word$edition$licType";
+    "Excel$edition$licType";
+    "PowerPoint$edition$licType";
+    "Outlook$edition$licType";
+    "Publisher$edition$licType";
+    "Access$edition$licType";
+    "HomeBusiness$($edition)Retail";
+    "HomeStudent$($edition)Retail"
+    )
     foreach ($productId in $productIds) {
         foreach ($arch in $archs){
-            Install-Office
+            Download-MSOffice
             $i++
         }
     }
@@ -125,7 +133,7 @@ if ($edition -match '365'){
     )
     foreach ($productId in $productIds) {
         foreach ($arch in $archs){
-            Install-Office
+            Download-MSOffice
             $i++
         }
     }
