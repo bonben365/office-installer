080da7ebb80659ede53ea523467d5aa663e7c24f
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-10 10:46:39 +0700

    Update uninstaller.ps1

--- uninstaller.ps1
+++ uninstaller.ps1
@@ -1,5 +1,6 @@
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/uninstaller | iex"
     break
 }
 
